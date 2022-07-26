a2b96272f5bc739faaf588a68773579b89c2710e
romantafelski <romantafelski18@gmail.com>
2022-07-26 18:24:25 -0400

    done for tonight

--- apps.js
+++ apps.js
@@ -1,7 +1,9 @@
 
-    const $h1 = $("<h1>").addClass("header").text("Roman Tafelski")
+    const $h1 = $("<h1>").addClass("h1").text("Roman Tafelski")
     $("body").append($h1)
-    const $h4 = $("<h4>").addClass("header").text("Student at General Assembly").appendTo("body")
+    const $h4 = $("<h4>").addClass("h4").text("Student at General Assembly").appendTo("body")
     const dropMenu = $("<button>").addClass("menu").text("Menu").appendTo("body")
+    const missionStatement = $("<h1>").addClass("missionStatement").text("Mission Statement").appendTo("body")
+    
 
 
