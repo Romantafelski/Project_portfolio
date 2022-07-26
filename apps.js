1fb3dfc26670d56a68e6ff21bb9a257016e9f13d
romantafelski <romantafelski18@gmail.com>
2022-07-26 17:38:57 -0400

    Tuesday background and header

--- apps.js
+++ apps.js
@@ -1,4 +1,7 @@
-const test = () => {
-    const headH1 = $("<h1>").addClass("header").text("Roman Tafelski").appendTo("body")
-}
-test()
+
+    const $h1 = $("<h1>").addClass("header").text("Roman Tafelski")
+    $("body").append($h1)
+    const $h4 = $("<h4>").addClass("header").text("Student at General Assembly").appendTo("body")
+    const dropMenu = $("<button>").addClass("menu").text("Menu").appendTo("body")
+
+
