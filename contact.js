c4baf6bd2c75173025563969c3cd821bf844b1f3
romantafelski <romantafelski18@gmail.com>
2022-08-01 12:56:59 -0400

    contact page upload

---
+++ contact.js
@@ -0,0 +1,34 @@
+
+    
+    const $h1 = $("<h1>").addClass("h1").text("Roman Tafelski")
+    $("body").append($h1)
+    const $h4 = $("<h4>").addClass("h4").text("Student at General Assembly").appendTo("body")
+    
+    $("<button>").addClass("menuButton").text("Menu").appendTo("body")
+    
+    $(".menu").hide()
+    const dropDown = () => {
+        $(".menu").toggle("slow")
+    }
+    $('.menuButton').on("click", dropDown)
+    //https://www.w3schools.com/jquery/event_hover.asp
+    $(".menuButton").hover(() => {
+        $("button").css("color", "rgb(91, 51, 120)")
+    }, () => {
+        $("button").css("color", "rgb(195, 194, 194)")
+    })
+    
+    $("<button>").addClass("contact").text("Contact Me").appendTo("body")
+    
+    const showEmail = $("<div>").addClass("email").text("RomanTafelski@gmail.com").hide().appendTo("body")
+    const showLinkedIn = $("<div>").addClass("textLink").text("linkedin.com/in/roman-tafelski-754b20105/").hide().appendTo("body")
+    
+    const contactBox = () => {
+        $(".email").toggle(1250)
+        $(".textLink").toggle(1250)
+    }
+    $(".contact").on("click", contactBox)
+    
+    
+    
+    
