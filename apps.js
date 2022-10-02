514eeebaec97a6f5ed463d6bdb5bff0d47513ef5
romantafelski <romantafelski18@gmail.com>
2022-10-02 13:50:02 -0400

    ;dlfkj

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
     $("body").addClass("background")
     
-    const $h1 = $("<h1>").addClass("h1").text("Roman Tafelski<br/> Student at General Assembly")
+    const $h1 = $("<h1>").addClass("h1").text("Roman Tafelski")
     $("body").append($h1)
     const $h4 = $("<h4>").addClass("h4").text("Student at General Assembly").appendTo("body")
    
